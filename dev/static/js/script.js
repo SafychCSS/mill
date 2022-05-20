4c2b308a285cbb0a8cf227bbf3b4328e39fe4c92
safych <rs@leads.su>
2022-05-20 12:34:28 +0300

    add micromodal js, change modal html, css

--- dev/static/js/script.js
+++ dev/static/js/script.js
@@ -1,9 +1,3 @@
-document.addEventListener('load', function () {
-    /* btn answer */
-    const btnAnswers = document.querySelectorAll('.btn-answer');
-    btnAnswers.forEach(btn => {
-        btn.addEventListener('click', () => {
-            // btn.classList.add('active');
-        });
-    });
+document.addEventListener('DOMContentLoaded', function () {
+    MicroModal.init();
 });
